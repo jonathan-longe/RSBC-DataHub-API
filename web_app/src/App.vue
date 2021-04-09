dbb8aedf973e889e051337abc1ebbcbcce5c3bb7
Jonathan Longe <jlonge@corebox.net>
2021-04-08 19:31:44 -0700

    basic RSI splash screen

---
+++ web_app/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div class="px-4 py-5 my-5 text-center">
+    <img class="d-block mx-auto mb-4" src="@/assets/BCID_RoadSafetyBC_logo_transparent.png" >
+    <div class="col-lg-12 mx-auto">
+      <p class="lead mb-4">Future home of digital prohibition forms for police.</p>
+    </div>
+  </div>
+
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: lightgray;
+  margin-top: 60px;
+}
+
+</style>

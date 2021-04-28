fec073bf32e9f7ef7d724690d149871816edd090
Jonathan Longe <jlonge@corebox.net>
2021-04-28 08:57:12 -0700

    replace hard-coded pdf template name

--- web_app/src/helpers/xfdf_generator.js
+++ web_app/src/helpers/xfdf_generator.js
@@ -10,30 +10,30 @@ function generate(pdf_filename, form_key_value_pairs) {
 
 function encode_template(pdf_filename) {
     console.log("pdf_filename", pdf_filename)
-    const pdf_template = require("@/assets/pdf/MV2906_12h_Suspension_2021-04-27.pdf")
+    const pdf_template = require("@/assets/pdf/" + pdf_filename)
     const base64_string = pdf_template.split(',')[1];
     console.log("pdf_template", base64_string)
     return base64_string
 }
 
 function get_xml(base64_encoded_template, form_key_value_pairs) {
-        const root = builder.create({version: "1.0", encoding: "UTF-8"})
-        .ins("xfa", "generator='AdobeDesigner_V7.0' APIVersion='2.2.4333.0'")
-        .ele('xdp:xdp', {'xmlns:xdp': 'http://ns.adobe.com/xdp/'})
-            .ele('xfa:datasets', {'xmlns:xfa': 'http://www.xfa.org/schema/xfa-data/1.0/'})
-                .ele('xfa:data');
-                    Object.entries(form_key_value_pairs).forEach( property => {
-                        console.log("property", property)
-                        root.ele(property[0]).txt(property[1]);
-                    });
-        root.up()
-            .up()
-        .ele('pdf', {xmlns: "http://ns.adobe.com/xdp/pdf/"})
-        .ele('document')
-            .ele('chunk').txt(base64_encoded_template).up()
-            .up()
+    let root = builder.create({version: "1.0", encoding: "UTF-8"})
+    .ins("xfa", "generator='AdobeDesigner_V7.0' APIVersion='2.2.4333.0'")
+    .ele('xdp:xdp', {'xmlns:xdp': 'http://ns.adobe.com/xdp/'})
+        .ele('xfa:datasets', {'xmlns:xfa': 'http://www.xfa.org/schema/xfa-data/1.0/'})
+            .ele('xfa:data');
+    Object.entries(form_key_value_pairs).forEach( property => {
+        console.log("property", property)
+        root.ele(property[0]).txt(property[1]);
+    });
+    root.up()
         .up()
-        .doc()
+    .ele('pdf', {xmlns: "http://ns.adobe.com/xdp/pdf/"})
+    .ele('document')
+        .ele('chunk').txt(base64_encoded_template).up()
+        .up()
+    .up()
+    .doc()
     const root_string = root.end({format: 'xml', prettyPrint: true})
     console.log("root_string", root_string )
     return root_string;
